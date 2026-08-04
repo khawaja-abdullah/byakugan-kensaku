40261fe846dfec9c879ad8b1c18caa73fa303090
khawaja-abdullah <khawaja.abdullah.ansar@gmail.com>
2026-08-04 10:30:00 +0500

    refactor: complete context load test with assertion

--- src/test/java/io/github/khawajaabdullah/byakugankensaku/ByakuganKensakuApplicationTests.java
+++ src/test/java/io/github/khawajaabdullah/byakugankensaku/ByakuganKensakuApplicationTests.java
@@ -1,13 +1,21 @@
 package io.github.khawajaabdullah.byakugankensaku;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class ByakuganKensakuApplicationTests {
 
+  @Autowired
+  private ApplicationContext applicationContext;
+
   @Test
-  void contextLoads() {
+  void shouldLoadContext() {
+    assertNotNull(applicationContext, "ApplicationContext should have been loaded");
   }
 
 }
